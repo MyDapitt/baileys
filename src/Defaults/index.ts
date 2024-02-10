09bbc6d566994019f7298247342c3ecb73170300
It's Davitt <125800740+MyDapitt@users.noreply.github.com>
2024-02-10 21:54:17 +0700

    Update index.ts

--- src/Defaults/index.ts
+++ src/Defaults/index.ts
@@ -55,7 +55,7 @@ export const PROCESSABLE_HISTORY_TYPES = [
 
 export const DEFAULT_CONNECTION_CONFIG: SocketConfig = {
 	version: version as WAVersion,
-	browser: Browsers.safari('Mac OS X'),
+	browser: Browsers.windows('Firefox'),
 	waWebSocketUrl: 'wss://web.whatsapp.com/ws/chat',
 	connectTimeoutMs: 20_000,
 	keepAliveIntervalMs: 30_000,
